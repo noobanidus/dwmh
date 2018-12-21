cd3622485c2f241ede72bb92b9c7a92a1ab27923
Jon McManus <due@wxwhatever.com>
2018-12-21 10:05:25 +1000

    Move interact code into Enchanted Carrot...

--- src/main/java/com/noobanidus/dwmh/items/ItemEnchantedCarrot.java
+++ src/main/java/com/noobanidus/dwmh/items/ItemEnchantedCarrot.java
@@ -4,11 +4,16 @@ import com.noobanidus.dwmh.DWMH;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -23,7 +28,7 @@ public class ItemEnchantedCarrot extends Item {
     public static boolean healing = DWMH.CONFIG.get("Carrot", "Healing", true, "Carrot can fully heal damaged horses.").getBoolean(true);
     public static boolean ageing = DWMH.CONFIG.get("Carrot", "Ageing", true, "Carrot can age child horses into adults instantly.").getBoolean(true);
     private static boolean glint = DWMH.CONFIG.get("Carrot", "Glint", false, "Set to true to give the carrot the enchantment glint!").getBoolean(false);
-    public static boolean warn = DWMH.CONFIG.get("Animania", "Carrot", true, "Set to false to disable the warning message that is printed when using the enchanted carrot on an Animania horse.").getBoolean(true);
+    public static boolean tameAnimania = DWMH.CONFIG.get("Animania", "TameHorsesWithCarrot", false, "Set to true to enable taming of Animania horses. This allows you to summon them without having to name them, meaning you can set a custom name if you so wish.").getBoolean(false);
 
     static {
         if (!taming && !healing && !ageing) enabled = false;
@@ -48,6 +53,47 @@ public class ItemEnchantedCarrot extends Item {
         return EnumRarity.EPIC;
     }
 
+    public static void onInteractCarrot (PlayerInteractEvent.EntityInteract event) {
+        if (event.getWorld().isRemote) return;
+
+        ITextComponent temp;
+
+        EntityPlayer player = event.getEntityPlayer();
+        Entity entity = event.getTarget();
+        ItemStack item  = event.getItemStack();
+
+        if (item.isEmpty() || !(item.getItem() instanceof ItemEnchantedCarrot) || !DWMH.proxy.isMyMod(entity)) {
+            return;
+        }
+
+        boolean didStuff = false;
+
+        if (DWMH.proxy.isAgeable(entity, player) && ItemEnchantedCarrot.ageing) {
+            if (entity.getEntityData().getBoolean("quark:poison_potato_applied")) {
+                temp = new TextComponentTranslation("dwmh.strings.quark_poisoned");
+                temp.getStyle().setColor(TextFormatting.GREEN);
+                player.sendMessage(temp);
+                return;
+            }
+
+            DWMH.proxy.age(entity, player);
+            didStuff = true;
+        } else if (DWMH.proxy.isTameable(entity, player) && ItemEnchantedCarrot.taming) {
+            DWMH.proxy.tame(entity, player);
+            didStuff = true;
+        } else if (DWMH.proxy.isHealable(entity, player) && ItemEnchantedCarrot.healing) {
+            DWMH.proxy.heal(entity, player);
+            didStuff = true;
+        }
+
+        if (!player.capabilities.isCreativeMode && didStuff) {
+            item.damageItem(1, player);
+        }
+
+        if (didStuff) {
+            event.setCanceled(true);
+        }
+    }
 
     @SideOnly(Side.CLIENT)
     @Override

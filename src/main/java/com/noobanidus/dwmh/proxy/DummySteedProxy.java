e7f8eb6bf01df32d9fab878c497db0b7bb9270c4
Jon McManus <due@wxwhatever.com>
2018-12-21 10:09:06 +1000

    Implement DummySteedProxy.
    This basically does nothing except return "false" for isLoaded, meaning
    that it is filtered out of the proxy listed and not consulted. The
    variable is instantiated with it, however, so it's possible to check if
    DWMH.animaniaProxy.isLoaded()

--- src/main/java/com/noobanidus/dwmh/proxy/DummySteedProxy.java
+++ src/main/java/com/noobanidus/dwmh/proxy/DummySteedProxy.java
@@ -1,4 +1,61 @@
 package com.noobanidus.dwmh.proxy;
 
-public class DummySteedProxy {
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.ITextComponent;
+
+public class DummySteedProxy implements ISteedProxy {
+    public boolean isTeleportable (Entity entity, EntityPlayer player) {
+        return false;
+    }
+
+    public boolean isListable (Entity entity, EntityPlayer player) {
+        return false;
+    }
+
+    // Carrot
+    public boolean isTameable (Entity entity, EntityPlayer player) {
+        return false;
+    }
+
+    public void tame (Entity entity, EntityPlayer player) {
+    }
+
+    public boolean isAgeable (Entity entity, EntityPlayer player) {
+        return false;
+    }
+
+    public void age (Entity entity, EntityPlayer player) {
+    }
+
+    public boolean isHealable (Entity entity, EntityPlayer player) {
+        return false;
+    }
+
+    public void heal (Entity entity, EntityPlayer player) {
+    }
+
+    // Not currently implemented
+    public boolean isBreedable (Entity entity, EntityPlayer player) {
+        return false;
+    }
+
+    public void breed (Entity entity, EntityPlayer player) {
+    }
+
+    public boolean isLoaded () {
+        return false;
+    }
+
+    public boolean isMyMod (Entity entity) {
+        return false;
+    }
+
+    public ITextComponent getResponseKey (Entity entity, EntityPlayer player) {
+         return null;
+    }
+
+    public String proxyName () {
+        return "dummy";
+    }
 }

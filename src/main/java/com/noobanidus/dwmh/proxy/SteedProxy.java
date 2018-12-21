13ee870935a4081d53fa0e416ac91871f93977e3
Jon McManus <due@wxwhatever.com>
2018-12-21 10:10:28 +1000

    Implement SteedProxy -- overall proxy.
    This retroactively considers all relevant instances of ISteedProxy that
    have been loaded during post-init. Vanilla is always last in case horses
    (like Animania) derive from AbstractHorse.
    
    All proxies are consulted to determine if the entity is "part" of their
    mod, and if so, its functions are used.

--- src/main/java/com/noobanidus/dwmh/proxy/SteedProxy.java
+++ src/main/java/com/noobanidus/dwmh/proxy/SteedProxy.java
@@ -1,4 +1,161 @@
 package com.noobanidus.dwmh.proxy;
 
-public class SteedProxy {
+import com.noobanidus.dwmh.DWMH;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.ITextComponent;
+
+public class SteedProxy implements ISteedProxy {
+    public static boolean onlyNamed = DWMH.CONFIG.get("Animania", "OnlyNamed", true, "Only list named Animania steeds.").getBoolean(true);
+
+    public boolean hasCustomName (Entity entity) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isMyMod(entity)) {
+                return proxy.hasCustomName(entity);
+            }
+        }
+
+        return false;
+    }
+
+    public String getCustomNameTag (Entity entity) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isMyMod(entity)) {
+                return proxy.getCustomNameTag(entity);
+            }
+        }
+
+        return "";
+    }
+
+    public void setCustomNameTag (Entity entity, String name) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isMyMod(entity)) {
+                proxy.setCustomNameTag(entity, name);
+                break;
+            }
+        }
+    }
+
+    public boolean isTeleportable (Entity entity, EntityPlayer player) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isTeleportable(entity, player)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isListable (Entity entity, EntityPlayer player) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isListable(entity, player)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Carrot
+    public boolean isTameable (Entity entity, EntityPlayer player) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isTameable(entity, player)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void tame (Entity entity, EntityPlayer player) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isMyMod(entity)) {
+                proxy.tame(entity, player);
+                return;
+            }
+        }
+    }
+
+    public boolean isAgeable (Entity entity, EntityPlayer player) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isAgeable(entity, player)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void age (Entity entity, EntityPlayer player) {
+         for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isMyMod(entity)) {
+                proxy.age(entity, player);
+                return;
+            }
+        }
+    }
+
+    public boolean isHealable (Entity entity, EntityPlayer player) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isHealable(entity, player)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void heal (Entity entity, EntityPlayer player) {
+         for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isMyMod(entity)) {
+                proxy.heal(entity, player);
+                return;
+            }
+        }
+    }
+
+    // Not currently implemented
+    public boolean isBreedable (Entity entity, EntityPlayer player) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isBreedable(entity, player)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void breed (Entity entity, EntityPlayer player) {
+         for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isMyMod(entity)) {
+                proxy.breed(entity, player);
+                return;
+            }
+        }
+    }
+
+    public ITextComponent getResponseKey (Entity entity, EntityPlayer player) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isMyMod(entity)) {
+                return proxy.getResponseKey(entity, player);
+            }
+        }
+
+        return null;
+    }
+
+    public boolean isMyMod (Entity entity) {
+        for (ISteedProxy proxy : DWMH.proxyList) {
+            if (proxy.isMyMod(entity)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public String proxyName () {
+        return "main";
+    }
 }

59ad91d56e0f250c1deb8e384abbafb7843090f7
Jon McManus <due@wxwhatever.com>
2019-01-01 20:38:42 +1000

    Revert "For the hell of it, mark everything as requiring restart."
    This reverts commit 54e0dad41090d67750b88a828bf1f08270ae5e24.
    
    From what I can gather the config GUI is designed to be
    integrated-server specific rather than allow changes to be made on an
    actual multiplayer server. Hence my mistake.
    
    Thus, these need to be tested fully in that setting & perhaps with "open
    to LAN".

--- src/main/java/com/noobanidus/dwmh/config/DWMHConfig.java
+++ src/main/java/com/noobanidus/dwmh/config/DWMHConfig.java
@@ -19,28 +19,23 @@ public class DWMHConfig {
     @Config.Name("Ocarina Settings")
     public static Ocarina Ocarina = new Ocarina();
     public static class Ocarina {
-        @Config.RequiresMcRestart
         @Config.Comment("Specify the maximum distance steeds can be summoned from (set to 0 for infinite). Excludes entities in unloaded chunks or different dimensions")
         @Config.RangeDouble(min=0d, max=100000d)
         @Config.Name("Maximum Summon Distance")
         public double maxDistance = 200d;
 
-        @Config.RequiresMcRestart
         @Config.Comment("Set to true to enable summoning of horses even if they are being ridden by someone else.")
         @Config.Name("Summon With Other Riders")
         public boolean otherRiders = false;
 
-        @Config.RequiresMcRestart
         @Config.Comment("Set to true to swap the effect of sneaking. Thus, right-clicking will list entities, while sneak-right-clicking will summon them.")
         @Config.Name("Swap Sneak Functionality")
         public boolean swap = false;
 
-        @Config.RequiresMcRestart
         @Config.Comment("Set to false to prevent setting a home when summoning steeds. This should prevent them wandering too far from where you summoned them to, but may cause interactions with other mods.")
         @Config.Name("Set Home Location When Summoned")
         public boolean home = true;
 
-        @Config.RequiresMcRestart
         @Config.Comment("Set to true to skip removing a home point when you dismount a horse. This may help with mods that set a home upon dismounting, as well. Otherwise, setting this to false will make horses believe their home is where you last summoned them to was.")
         @Config.Name("Skip Clearing Home on Dismount")
         public boolean skipDismount = false;
@@ -49,57 +44,47 @@ public class DWMHConfig {
         @Config.Name("Functionality")
         public Functionality functionality = new Functionality();
         public class Functionality {
-            @Config.RequiresMcRestart
             @Config.Comment("Specify a cooldown for each usage of the Ocarina, or specify 0 to disable")
             @Config.Name("Ocarina Cooldown Duration")
             public int cooldown = 0;
 
-            @Config.RequiresMcRestart
             @Config.Comment("Specify the maximum durability of the Ocarina. One horse summoned costs one durability. Set to 0 to disable durability.")
             @Config.Name("Maximum Ocarina Durability")
             public int maxUses = 0;
 
-            @Config.RequiresMcRestart
             @Config.Comment("Specify the item that can be used to repair the Ocarina in an anvil. Items with NBT are not supported. Format mod:item:metadata (use \"minecraft\" for vanilla items), use 0 for no meteadata.")
             @Config.Name("Ocarina Repair Item")
             public String repairItem = "minecraft:golden_carrot:0";
 
-            @Config.RequiresMcRestart
             @Config.Comment("Specify the item to consume from the player's inventory before summoning a horse. Format: mod:item:metadata (use \"minecraft\" for vanilla items), use 0 for no metadata. Items with NBT are not supported.")
             @Config.Name("Summon Item")
             public String summonItem = "minecraft:carrot:0";
 
-            @Config.RequiresMcRestart
             @Config.Comment("Specify the quantity of the item to consume from the player's inventory before summoning a horse. Set to 0 to consume nothing.")
             @Config.Name("Summon Cost")
             public int summonCost = 0;
         }
 
-        @Config.Comment("Options related to audio and text output of the Ocarina. While these affect the client, the values on the server will override anything on the client.")
+        @Config.Comment("Options related to audio and text output of the Ocarina")
         @Config.Name("Responses")
         public Responses responses = new Responses();
         public class Responses {
-            @Config.RequiresMcRestart
             @Config.Comment("Set to true to disable messages when teleporting a horse to you.")
             @Config.Name("Disable Summoned Messages")
             public boolean quiet = false;
 
-            @Config.RequiresMcRestart
             @Config.Comment("Set to true to compact multiple horse-summoned messages into one.")
             @Config.Name("Combine Summoned Messages")
             public boolean simple = false;
 
-            @Config.RequiresMcRestart
             @Config.Comment("Set to false to disable showing the distance and direction a horse is away from you")
             @Config.Name("Disable Distance/Direction")
             public boolean distance = true;
 
-            @Config.RequiresMcRestart
             @Config.Comment("Set to false to disable sounds being played when the Ocarina is use. These sounds are played on the PLAYERS channel.")
             @Config.Name("Disable Ocarina Tunes")
             public boolean sounds = true;
 
-            @Config.RequiresMcRestart
             @Config.Comment("The delay in seconds between uses of the Ocarina causing a sound event. The Ocarina will still trigger, but silently, during this delay.")
             @Config.Name("Ocarina Sound Delay")
             public int soundDelay = 5;
@@ -115,23 +100,20 @@ public class DWMHConfig {
         @Config.Name("Enable Carrot")
         public boolean enabled = true;
 
-        @Config.Comment("Settings related to the durability of the Enchanted Carrot.")
+        @Config.Comment("Settings related to the durability of the EnchantedCarrot.")
         @Config.Name("Durability settings")
         public Durability durability = new Durability();
         public class Durability {
-            @Config.RequiresMcRestart
             @Config.RangeInt(min = 1)
             @Config.Comment("Maximum number of uses before the enchanted EnchantedCarrot is destroyed or, with the unbreakable setting, becomes unusable")
             @Config.Name("Maximum Carrot Durability")
             public int maxUses = 30;
 
-            @Config.RequiresMcRestart
-            @Config.Comment("Set to true to prevent the Enchanted Carrot from breaking when reaching 0 durability")
+            @Config.Comment("Set to true to prevent the EnchantedCarrot from breaking when reaching 0 durability")
             @Config.Name("Unuseable At Minimum Durability")
             public boolean unbreakable = false;
 
-            @Config.RequiresMcRestart
-            @Config.Comment("Specify the item that can be used to repair the Enchanted Carrot in an anvil. Items with NBT are not supported. Format: mod:item:metadata. Use \"minecraft\" for vanilla items, and 0 if no metadata is specified.")
+            @Config.Comment("Specify the item that can be used to repair the EnchantedCarrot in an anvil. Items with NBT are not supported. Format: mod:item:metadata. Use \"minecraft\" for vanilla items, and 0 if no metadata is specified.")
             @Config.Name("Carrot Repair Item")
             public String repairItem = "minecraft:gold_block:0";
         }
@@ -140,28 +122,24 @@ public class DWMHConfig {
         @Config.Name("Effects")
         public CarrotEffects effects = new CarrotEffects();
         public static class CarrotEffects {
-            @Config.RequiresMcRestart
             @Config.Comment("Set to false to prevent the automatic taming of rideable entities")
             @Config.Name("Enable Eligible Entity Taming")
             public boolean taming = true;
 
-            @Config.RequiresMcRestart
             @Config.Comment("Set to false to prevent the healing to full of injured rideable entities")
             @Config.Name("Enable Eligible Entity Healing")
             public boolean healing = true;
 
-            @Config.RequiresMcRestart
             @Config.Comment("Set to false to prevent the aging to adulthood of rideable child entities")
             @Config.Name("Enable Eligible Entity Aging")
             public boolean aging = true;
 
-            @Config.RequiresMcRestart
             @Config.Comment("Set to false to prevent putting tamed, adult rideable entities into \"breeding\" mode")
             @Config.Name("Enable Eligible Entity Breeding")
             public boolean breeding = true;
         }
 
-        @Config.Comment("Set to true to enable the enchantment glint. Useful if you are using a texture pack that overrides the Carrot animation. (Client-side only)")
+        @Config.Comment("Set to true to enable the enchantment glint. Useful if you are using a texture pack that overrides the Carrot animation.")
         @Config.Name("Enable Enchantment Glint")
         public boolean glint = false;
     }
@@ -200,11 +178,9 @@ public class DWMHConfig {
         @Config.Name("Animania Settings")
         public Animania Animania = new Animania();
         public class Animania {
-            @Config.RequiresMcRestart
             @Config.Comment("Specify list of Animania classes that are considered steeds. Use /dwmh entity while targetting to get the full name")
             public String[] classes = new String[]{"com.animania.common.entities.horses.EntityMareBase", "com.animania.common.entities.horses.EntityStallionBase"};
 
-            @Config.RequiresMcRestart
             @Config.Comment("Set to false to enable the listing of every Animania entity, not just those you are able to summon. This may produce an excess number of entries.")
             @Config.Name("Only Named")
             public boolean onlyNamed = true;
@@ -214,7 +190,6 @@ public class DWMHConfig {
         @Config.Name("ZAWA Settings")
         public ZAWA ZAWA = new ZAWA();
         public class ZAWA {
-            @Config.RequiresMcRestart
             @Config.Comment("Specify list of ZAWA Rebuilt classes that are considered steeds. Use /dwmh entity while targetting to get the full name")
             public String[] classes = new String[]{"org.zawamod.entity.land.EntityAsianElephant", "org.zawamod.entity.land.EntityGaur", "org.zawamod.entity.land.EntityGrevysZebra", "org.zawamod.entity.land.EntityOkapi", "org.zawamod.entity.land.EntityReticulatedGiraffe"};
         }
@@ -223,13 +198,11 @@ public class DWMHConfig {
         @Config.Name("Mo Creatures Settings")
         public MoCreatures MoCreatures = new MoCreatures();
         public class MoCreatures {
-            @Config.RequiresMcRestart
             @Config.Comment("Specify list of entity translation keys which should be modified to insert spaces (where relevant)")
             public String[] entities = new String[]{"entity.mocreatures:blackbear.name", "entity.mocreatures:grizzlybear.name", "entity.mocreatures:komododragon.name", "entity.mocreatures:petscorpion.name", "entity.mocreatures:wildhorse.name", "entity.mocreatures:wildpolarbear.name"};
         }
     }
 
-    @Config.RequiresMcRestart
     @Config.Comment("Specify a blacklist of entities that should always be ignored, even if generally loaded by their proxy.")
     @Config.Name("Entity Blacklist")
     public static String[] blacklist = new String[]{};

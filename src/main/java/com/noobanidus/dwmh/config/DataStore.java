fd55f271a7c115db66de3347f119c4919de2cb2c
Jon McManus <due@wxwhatever.com>
2019-02-15 16:55:11 +1000

    Also store configuration names.
    This could potentially become flimsy but outside of invidual switch
    statements, I can't really imagine any way to do this efficiently.

--- src/main/java/com/noobanidus/dwmh/config/DataStore.java
+++ src/main/java/com/noobanidus/dwmh/config/DataStore.java
@@ -1,18 +1,17 @@
-package com.noobanidus.dwmh.proxy;
+package com.noobanidus.dwmh.config;
 
 import com.google.common.collect.Lists;
-import com.noobanidus.dwmh.config.ConfigHandler;
-import com.noobanidus.dwmh.config.DWMHConfig;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class DataStore {
     private static Map<String, String> PROXY_LIST = new HashMap<>();
+    private static Map<String, String> CONFIG_LIST = new HashMap<>();
     private static Map<String, Set<String>> CLASS_REFERENCE = new HashMap<>();
 
     static {
-        DataStore.putAll("animania", "com.noobanidus.dwmh.proxy.steeds.AnimaniaProxy",
+        DataStore.putAll(PROXY_LIST, "animania", "com.noobanidus.dwmh.proxy.steeds.AnimaniaProxy",
                 "mocreatures", "com.noobanidus.dwmh.proxy.steeds.MOCProxy",
                 "zawa", "com.noobanidus.dwmh.proxy.steeds.ZawaProxy",
                 "ultimate_unicorn_mod", "com.noobanidus.dwmh.proxy.steeds.UnicornProxy",
@@ -21,6 +20,17 @@ public class DataStore {
                 "dragonmounts", "com.noobanidus.dwmh.proxy.steeds.DragonMountProxy",
                 "varodd", "com.noobanidus.dwmh.proxy.steeds.VaroddProxy",
                 "moolands", "com.noobanidus.dwmh.proxy.steeds.MoolandProxy");
+
+        DataStore.putAll(CONFIG_LIST,
+                "Animania", "animania",
+                "Mo Creatures", "mocreatures",
+                "ZAWA Rebuilt", "zawa",
+                "Ultimate Unicorn Mod", "ultimate_unicorn_mod",
+                "Atum", "atum",
+                "Ice & Fire", "iceandfire",
+                "Dragon Mounts 2", "dragonmounts",
+                "Various Oddities", "varodd",
+                "Moolands", "moolands");
 
         CLASS_REFERENCE.put("zawa", new HashSet<>());
         CLASS_REFERENCE.put("animania", new HashSet<>());
@@ -31,10 +41,10 @@ public class DataStore {
         CLASS_REFERENCE.put("blacklist", new HashSet<>());
     }
 
-    private static void putAll(String... input) {
+    private static void putAll(Map<String, String> map, String... input) {
         assert input.length % 2 == 0;
         for (int i = 0; i < input.length; i += 2) {
-            PROXY_LIST.put(input[i], input[i + 1]);
+            map.put(input[i], input[i + 1]);
         }
     }
 
@@ -42,21 +52,27 @@ public class DataStore {
         return PROXY_LIST.entrySet().stream().map((a) -> new Proxy(a.getKey(), a.getValue())).collect(Collectors.toList());
     }
 
-    public static List<String> mods () {
+    public static List<String> mods() {
         return Lists.newArrayList(PROXY_LIST.keySet());
     }
 
-    public static Set<String> set (String setName) {
+    public static Set<String> set(String setName) {
         if (CLASS_REFERENCE.containsKey(setName)) return CLASS_REFERENCE.get(setName);
 
-        return new HashSet<>();
+        return null;
+    }
+
+    public static String proxy (String configName) {
+        if (CONFIG_LIST.containsKey(configName)) return CONFIG_LIST.get(configName);
+
+        return null;
     }
 
     public static class Proxy {
         private String modId;
         private String classPath;
 
-        public Proxy (String modId, String classPath) {
+        public Proxy(String modId, String classPath) {
             this.modId = modId;
             this.classPath = classPath;
         }

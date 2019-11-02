2d6125e55282910bfc247dec21ab8ecb2c8a2214
Jon McManus <due@wxwhatever.com>
2019-11-02 19:58:53 +1000

    Handle sidedness issues.

--- src/main/java/noobanidus/mods/dwmh/network/GetName.java
+++ src/main/java/noobanidus/mods/dwmh/network/GetName.java
@@ -5,6 +5,8 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.network.NetworkEvent;
 
 import java.util.function.Supplier;
@@ -20,19 +22,26 @@ public class GetName {
     this.entityId = entityId;
   }
 
+  public int getEntityId() {
+    return entityId;
+  }
+
   public void encode(PacketBuffer buffer) {
     buffer.writeInt(this.entityId);
   }
 
   public void handle(Supplier<NetworkEvent.Context> context) {
-    context.get().enqueueWork(() -> {
-      PlayerEntity player = Minecraft.getInstance().player;
-      World world = player.world;
-      Entity target = world.getEntityByID(entityId);
-      if (target != null) {
-        SendName packet = new SendName(target.getName());
-        Networking.sendToServer(packet);
-      }
-    });
+    context.get().enqueueWork(() -> handle(this, context));
+  }
+
+  @OnlyIn(Dist.CLIENT)
+  private static void handle(GetName message, Supplier<NetworkEvent.Context> context) {
+    PlayerEntity player = Minecraft.getInstance().player;
+    World world = player.world;
+    Entity target = world.getEntityByID(message.getEntityId());
+    if (target != null) {
+      SendName packet = new SendName(target.getName());
+      Networking.sendToServer(packet);
+    }
   }
 }
